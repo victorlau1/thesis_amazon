81e51cd87a7502279221211ca5573300375b2fe0
Victor Lau <lau.victor.w@gmail.com>
2017-12-26 19:55:11 -0800

    add endpoints

--- server/views.py
+++ server/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .models import Carts, Items
+from .serializers import CartSerializer, ItemSerializer
+from .services import cart_checkout
 
-# Create your views here.
+class CartViewSet(viewsets.ModelViewSet):
+  """
+  API endpoint that handles incoming post requests for carts
+  """
+  queryset = Carts.objects.all()
+  serializer_class = CartSerializer
+
+class InventoryViewSet(viewsets.ModelViewSet):
+  """
+  API endpoint that handles incoming post requests for validating inventory levels
+  """
+
+
