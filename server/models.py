4900f564c9ce3847d671e98f2cfcc86efff4bc3a
Victor Lau <lau.victor.w@gmail.com>
2017-12-26 19:54:46 -0800

    update models

--- server/models.py
+++ server/models.py
@@ -1,17 +1,30 @@
 from django.db import models
-
 from django.utils import timezone
 
-# Create your models here.
-class cart_information(models.Model):
-  """ Cart Data Received From Clients Microservice
-    
+
+class Carts(models.Model):
+  """ 
+    Cart Data Received From Clients Microservice
   """
   cart_id = models.IntegerField(primary_key=True)
-  item_name = models.CharField(max_length=200)
-  price = models.IntegerField()
+  user_id = models.IntegerField()
+  subtotal = models.FloatField()
+  shipping_cost = models.FloatField()
   ordered_at = timezone.now()
-  valid_purchase = models.BooleanField(default=False)
 
   def __str__(self):
-    return self
+    return {(field.name, field.value_to_string(self)) for field in Carts._meta.fields}
+
+  
+class Items(models.Model):
+  """
+    Item per Cart list; Combo Key is cart_id, and item_id. 
+  """
+  class Meta:
+    unique_together = (('cart_id','id'))
+
+  cart_id = models.ForeignKey(Carts, db_constraint=False, on_delete=models.CASCADE)
+  item_price = models.FloatField()
+  shipping_cost = models.FloatField()
+  quantity = models.IntegerField()
+  valid_purchase = models.BooleanField(default=False)
